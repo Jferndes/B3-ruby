861f11387ec64a1431a39462502cd121e8f700be
Joel <joel.fernandes@edu.ece.fr>
2025-03-03 10:58:06 +0100

    10/02

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_28_122045) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_28_135423) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,5 +28,9 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_28_122045) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_weight_entries_on_user_id"
   end
+
+  add_foreign_key "weight_entries", "users"
 end
